d747cae28b0eaba9df1d4d4aeff7cc39872d00c2
Alex Mackey <alexjmackey@gmail.com>
2012-09-23 16:05:56 +1000

    First stab at retry Ajax wrapper. Defaults config and contains backoff function

--- retryAjax.js
+++ retryAjax.js
@@ -1,29 +1,56 @@
+/*
+Author: Alex Mackey
+Site: simpleisbest.co.uk
+Description: First stab at wrapper for jQuery AJAX method to retry requests 
+*/
 
-(function($) {
 
-	$.extend( {
+(function($, undefined) {
 
-		retryAjax: function(arguments, retryAttempts)
-	    {
-	    	console.log('calling');
+	"option strict";
 
-	    	var _ajax = $.ajax;
-	    	var retryCount=0;
+	var retryCounter=1,
+	 	backoffInterval=100;
 
-	    	for (i=0; i<retryAttempts; i++){
+	$.ajaxWithRetries=function(options, retryConfig) {
+	
+		var 
+		originalErrorFunc=options.error,
+		config={
+			retries: 3,
+			backoff: false,
+			backOffFunc: function(currentInterval){
+				return currentInterval *2;
+			}
+		};
+		
+		$.extend(config, retryConfig);
+		
+		options.error=function(){
 
-	    		console.log('attempt ' + i);
+			if(retryCounter===config.retries){					
+				
+				if(typeof originalErrorFunc !== "undefined"){
+					originalErrorFunc();
+				}		
+				
+			}
+			else{
+								
+				retryCounter++;
+				
+				if(config.exponentialBackoff){
+					backoffInterval = retryCounter===0 ? backoffInterval : 	config.backOffFunc(backoffInterval);
+				}
+				
+				setTimeout(function(){$.ajax(options)}, backoffInterval);
+			}
+				
+		}
 
-	    		var ret= _ajax.apply(this, arguments);	
+		return $.ajax(options);
 
-	    		debugger;
-	    	}
-	    	
-	    	return ret;
-	    }
-
-	});
-
+	};
 	
 
-})(jQuery);
+})(jQuery, undefined);
